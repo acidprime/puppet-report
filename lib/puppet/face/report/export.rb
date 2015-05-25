7b96b24976cf320794ac0e677d7660129a83e371
Zack Smith <zack@puppetlabs.com>
2015-05-25 00:58:50 -0700

    Add simple csv output

--- lib/puppet/face/report/export.rb
+++ lib/puppet/face/report/export.rb
@@ -18,11 +18,10 @@ Puppet::Face.define(:report, '0.0.1') do
       default_to { false }
     end
 
-    option "--status RESOURCE" do
+    option "--status REPORT_STATUS" do
       summary "Filter reports by status {failed,unchanged,changed,noop}"
       default_to { 'failed' }
     end
-
 
     description <<-'EOT'
       This is a simple wrapper to connect to puppetdb for exported records
@@ -37,63 +36,34 @@ Puppet::Face.define(:report, '0.0.1') do
     EOT
 
     when_invoked do |options|
-      output = [ {'certname' => 'end-time'} ]
       connection = Puppet::Network::HttpPool.http_instance(Puppet::Util::Puppetdb.server,Puppet::Util::Puppetdb.port)
+
+
       query = ["and",["=",["node","active"],true],["=","status",options[:status]]] 
+
       json_query = URI.escape(query.to_json)
-      unless filtered = PSON.load(connection.request_get("/v4/reports/?query=#{json_query}", {"Accept" => 'application/json'}).body)
+      unless reports = PSON.load(connection.request_get("/v4/reports/?query=#{json_query}", {"Accept" => 'application/json'}).body)
         raise "Error parsing json output of puppet search #{filtered}"
       end
-      output << filtered.map { |report| Hash[report['certname'] => DateTime.parse(report['end-time']).rfc2822]}
-      output.flatten
+      reports
     end
 
-    when_rendering :console do |output,options|
-      if output.empty?
+    when_rendering :console do |reports,options|
+      if reports.empty?
         Puppet.notice("No reports found")
       end
-      padding = '  '
-      headers = {
-        'node_name'   => 'certname',
-        'end_time'     => 'end-time',
-      }
-
-      min_widths = Hash[ *headers.map { |k,v| [k, v.length] }.flatten ]
-      min_widths['node_name'] = min_widths['end_time'] = 40
-
-      min_width = min_widths.inject(0) { |sum,pair| sum += pair.last } + (padding.length * (headers.length - 1))
-
-      terminal_width = [Puppet::Util::Terminal.width, min_width].max
-
-      highlight = proc do |color,s|
-        c = colorize(color, s)
-        c
+      output = []
+      reports.each do |report|
+        output << [
+                  report['certname'],
+                  report['environment'],
+                  report['configuration-version'],
+                  DateTime.parse(report['end-time']).rfc2822,
+                  report['end-time'],
+                  report['hash'],
+                  ].join(',')
       end
-      n = 0
-      output.collect do |results|
-
-        columns = results.inject(min_widths) do |node_name,end_time|
-          {
-            'node_name'  => node_name.length,
-            'end_time'   => end_time.length,
-          }
-        end
-
-        flex_width = terminal_width - columns['node_name'] - columns['end_time'] - (padding.length * (headers.length - 1))
-
-        format = %w{node_name end_time}.map do |k|
-          "%-#{ [ columns[k], min_widths[k] ].max }s"
-        end.join(padding)
-        results.map do |node_name,end_time|
-          n += 1
-          Puppet.debug("#{options.inspect}")
-          if n.odd?
-            (options[:highlight] && highlight[ :hwhite,format % [ node_name, end_time ] ] || format % [ node_name, end_time ])
-          else
-            (options[:highlight] && highlight[ :white,format % [ node_name, end_time ] ] || format % [ node_name, end_time ])
-          end
-        end.join
-      end
+      output
     end
   end
 end
